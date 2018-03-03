2bb4cce036bff391fd5bc1ec4d361fe0c1ef84e5
Madison Laughlin <laughmj5@uw.edu>
2018-03-03 14:32:38 -0800

    Made scatter plot

--- server.R
+++ server.R
@@ -1,15 +1,24 @@
 library(shiny)
 library(ggplot2)
+library('dplyr')
 source('merging.R')
 
 my.server <- function(input, output){
-  output$wg.plot <- renderPlot ({
-    wg.plot <- ggplot(data = wealthy.generosity) +
-      geom_jitter(mapping = aes(x = paste0("Economy..GDP.per.Capita..", input$year),
-                                y = paste0("Generosity.", input$year),
-                                color = 'Region')) +
-      geom_smooth(mapping =  aes(x = paste0("Economy..GDP.per.Capita..", input$year),
-                                 y = paste0("Generosity.", input$year)))
+  output$graph <- renderPlot ({
+    wealthy.generosity1 <- filter(wealthy.generosity, Region == input$region)
+    if(input$chosen.year == "15") {
+      wg.plot <- ggplot(data = wealthy.generosity1, mapping = aes(x = "Economy..GDP.per.Capita..15",
+                                                                 y = "Generosity.15")) +
+        geom_jitter()
+    } else if(input$chosen.year == "16") {
+      wg.plot <- ggplot(data = wealthy.generosity1, aes(x = "Economy..GDP.per.Capita..16",
+                                                       y = "Generosity.16")) +
+        geom_jitter()
+    } else {
+      wg.plot <- ggplot(data = wealthy.generosity1, aes(x = "Economy..GDP.per.Capita..17",
+                                                       y = "Generosity.17")) +
+        geom_jitter()
+    }
     return(wg.plot)
   })
 }
--- ui.R
+++ ui.R
@@ -4,11 +4,16 @@ source('merging.R')
 my.ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(
-      radioButtons(year, "Select Year for World Happiness Report", selected = "2015", choiceNames = 
-                    c("2015", "2016", "2017"), choiceValues = c("15", "16", "17"))
+      radioButtons("chosen.year", label = "Select Year for World Happiness Report", 
+                   choices = c("15", "16", "17")),
+      selectInput("region", label = "Select Region to View", 
+                  choices = c("Australia and New Zealand", "Central and Eastern Europe", "Eastern Asia",
+                              "Latin America and Caribbean", "Middle East and Northern Africa", 
+                              "North America", "Southeastern Asia", "Southern Asia", "Sub-Saharan Africa",
+                              "Western Europe"))
     ),
     mainPanel(
-      plotOutput(wg.graph, "90%", "90%")
+      plotOutput('graph')
     )
   )
 )
