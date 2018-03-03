0bbbbc20875ba73547038060efd00782cbe2d073
Madison Laughlin <laughmj5@uw.edu>
2018-03-03 13:35:58 -0800

    Added plot to the Shiny app (still has some bugs)

--- server.R
+++ server.R
@@ -1,7 +1,17 @@
 library(shiny)
+library(ggplot2)
+source('merging.R')
 
 my.server <- function(input, output){
-  
+  output$wg.plot <- renderPlot ({
+    wg.plot <- ggplot(data = wealthy.generosity) +
+      geom_jitter(mapping = aes(x = paste0("Economy..GDP.per.Capita..", input$year),
+                                y = paste0("Generosity.", input$year),
+                                color = 'Region')) +
+      geom_smooth(mapping =  aes(x = paste0("Economy..GDP.per.Capita..", input$year),
+                                 y = paste0("Generosity.", input$year)))
+    return(wg.plot)
+  })
 }
 
 shinyServer(my.server)
--- ui.R
+++ ui.R
@@ -1,7 +1,16 @@
 library(shiny)
+source('merging.R')
 
 my.ui <- fluidPage(
-  
+  sidebarLayout(
+    sidebarPanel(
+      radioButtons(year, "Select Year for World Happiness Report", selected = "2015", choiceNames = 
+                    c("2015", "2016", "2017"), choiceValues = c("15", "16", "17"))
+    ),
+    mainPanel(
+      plotOutput(wg.graph, "90%", "90%")
+    )
+  )
 )
 
 shinyUI(my.ui)
